abc22e231618e1886718840e323797b36937eec5
HoodFast <rabota-trassa@mail.ru>
2025-01-26 13:04:05 +0300

    commit

---
+++ src/bloggers/bloggers.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { BloggersController } from './bloggers/bloggers.controller';
+
+@Module({
+  controllers: [BloggersController]
+})
+export class BloggersModule {}
