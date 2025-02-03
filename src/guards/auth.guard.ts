bb6f5562c7d52799635194534c53fde64d0f1856
HoodFast <rabota-trassa@mail.ru>
2025-02-03 22:37:23 +0300

    commit

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor() {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    if (!request.headers.authorization) {
+      throw new UnauthorizedException('auth guard no headers');
+    }
+
+    const auth = request.headers.authorization;
+    const type = auth.split(' ')[0];
+    if (type !== 'Basic')
+      throw new UnauthorizedException('type is not a Basic');
+    const authPayload = auth.split(' ')[1];
+    const decodePayload = Buffer.from(authPayload, 'base64').toString();
+    if (decodePayload !== 'admin:qwerty')
+      throw new UnauthorizedException('login or pass is incorect');
+    return true;
+  }
+}
